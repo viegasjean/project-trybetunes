4f53854dd3ba5fd4f630ca59891029516cd1c22e
Jean Viegas <jeanmelg@gmail.com>
2022-01-17 14:38:45 -0300

    requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -29,6 +30,9 @@ export default class Header extends React.Component {
     const { userName, isLoading } = this.state;
     return (
       <header data-testid="header-component">
+        <Link data-testid="link-to-search" to="/search"> Search </Link>
+        <Link data-testid="link-to-favorites" to="/favorites"> Favorites </Link>
+        <Link data-testid="link-to-profile" to="/profile"> Profile </Link>
         {
           isLoading ? <Loading /> : <h4 data-testid="header-user-name">{userName}</h4>
         }
