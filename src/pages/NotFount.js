427495bce9395a51742d9072b677fe11540cacb6
Jean Viegas <jeanmelg@gmail.com>
2022-01-17 11:12:04 -0300

    inicia projeto, requisito 1

---
+++ src/pages/NotFount.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <h1>404 OPS - Página não encontrada!</h1>
+      </div>
+    );
+  }
+}
